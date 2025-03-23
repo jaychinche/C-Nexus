15749ad298e8af533600fb7d62d0d1581e699314
Jay chinche <94739010+jaychinche@users.noreply.github.com>
2025-03-23 23:28:21 +0530

    Update TutorialReferenceArticle.jsx

--- src/components/TutorialReferenceArticle.jsx
+++ src/components/TutorialReferenceArticle.jsx
@@ -20,7 +20,7 @@ const BackgroundColorContainer = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "center",
   position: "relative",
-  backgroundImage: "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+   backgroundImage:"radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
   backgroundRepeat: "no-repeat",
   ...theme.applyStyles("dark", {
     backgroundImage: "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
