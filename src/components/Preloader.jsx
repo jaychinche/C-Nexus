af75f795830e71a55b02f4f69dfa2f6ebaaf3adf
Jay chinche <94739010+jaychinche@users.noreply.github.com>
2025-03-23 23:22:30 +0530

    Update Preloader.jsx

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -11,7 +11,7 @@ const BackgroundColorContainer = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "center",
   position: "relative",
-  backgroundImage: "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+ backgroundImage:"radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))", // Default Dark Theme
   backgroundRepeat: "no-repeat",
   ...theme.applyStyles("dark", {
     backgroundImage: "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
